e68456b564cba1f101f513e666477bbdf6a98271
simonkassel <kassel.simon@gmail.com>
2020-01-07 12:42:09 -0500

    add comments to benchmark script

--- README.md
+++ README.md
@@ -4,9 +4,36 @@ Benchmark model for DrivenData Open Cities AI Challenge
 This repository is designed to be a resource for contestants of [DrivenData's Open Cities AI Challenge](https://www.drivendata.org/competitions/60/building-segmentation-disaster-resilience/page/150/). The objective of this challenge is to develop a machine learning model that can segment out buildings from aerial imagery of several African cities. The code and documentation contained here demonstrate how to train a model and make predictions on the competition data using [Raster Vision](https://rastervision.io/).
 
 ## Raster Vision
-Raster Vision is an python framework that makes it easy to implement existing backend deep learning frameworks for computer vision tasks that use satellite, aerial and other large format imagery. To learn more about what Raster Vision can do, check out [the docs](https://docs.rastervision.io/en/0.9/) or [this blog post](https://www.azavea.com/blog/2018/10/18/raster-vision-release/) announcing its release. If you are interested in tutorials on using Raster Vision outside the context of this competition, there are several examples in the [raster-vision-examples repo](https://github.com/azavea/raster-vision-examples).
+Raster Vision (rv) is an python framework that makes it easy to implement existing backend deep learning frameworks for computer vision tasks that use satellite, aerial and other large format imagery. To learn more about what Raster Vision can do, check out [the docs](https://docs.rastervision.io/en/0.9/) or [this blog post](https://www.azavea.com/blog/2018/10/18/raster-vision-release/) announcing its release. If you are interested in tutorials on using Raster Vision outside the context of this competition, there are several examples in the [raster-vision-examples repo](https://github.com/azavea/raster-vision-examples).
 
 ## Instructions
 
-### Getting started
+An rv experiment can be run either locally or on [AWS Batch](https://aws.amazon.com/batch/) but to train a deep learning model you will need access to GPUs. This example demonstrates how to train a model on AWS Batch. Using batch is beneficial not only because we gain access to GPUs but it also allows us to split some of the tasks across many instances and speed up the process. If you would like to reproduce this example on AWS Batch, begin by following the configuration instructions within the [Raster Vision AWS repo](https://github.com/azavea/raster-vision-aws).
 
+1. *Download test STAC*
+PyStac is able to access STAC catalogs on s3 but it is much faster to read them locally. It will not be a problem to read the training STAC directly from the DrivenData hosted s3 bucket but given that there are so many items in the test set (over 11,000), we recommend that you download the STAC and read it locally. It will end up being much faster than trying to read from s3 each time. 
+
+- Download the test data from [the data download page](https://www.drivendata.org/competitions/60/building-segmentation-disaster-resilience/data/) and unpack it into a 'test' directory within the data folder in this repo
+
+2. *Build and publish docker resources*
+This experiment is designed to run within a pre-configured docker environment. You can build, publish and run the docker resources using scripts within the `docker` directory.
+
+Build the docker image:
+```
+./docker/build
+```
+This will create a local docker image called 'raster-vision-wb-africa' that includes the code within the `benchmark` module, which you will need to run this experiment. 
+
+If you would like to run the workflow on remotely you will need to publish the image to AWS Batch. 
+- Edit `docker/publish_image` to reference the ECR cpu and gpu repos that you created during the [Raster Vision AWS setuo](https://github.com/azavea/raster-vision-aws#raster-vision-aws-batch-runner-setup).
+Publish your docker image to both gpu and cpu repos:
+```
+./docker/publish_image
+```
+Both images will be tagged with `world-bank-challenge`. When/if you make changes to the aux commands you will need to repeat this process (i.e. rebuild the docker image and publish the updated version to ecr).
+
+Run the docker container with the run script:
+```
+./docker/run --aws
+```
+The `--aws` flag forwards your AWS credentials which you will need if you plan to run any jobs remotely or access data on s3. However if you store your data and train the model locally you can run the script without it.
--- benchmark/experiments/benchmark.py
+++ benchmark/experiments/benchmark.py
@@ -47,56 +47,56 @@ class BenchmarkExperiment(rv.ExperimentSet):
         # For this experiment, we are interested in making predictions on two different
         # sets of images:
         #   1. The validation set, which we subset from the tier 1 training data. These
-        #      scenes have labels associated with them and will be used to calculate 
+        #      scenes have labels associated with them and will be used to calculate
         #      validation metrics during the eval stage.
-        #   2. The test set, which come from the `test` stac. These will not be used for 
-        #      model evaluation because we don't have the labels for them. Instead, we 
-        #      will submit the test set predictions to the competition site. 
-        #   
-        # We will need to make scenes for each of the 11,481 test chips. It is possible 
+        #   2. The test set, which come from the `test` stac. These will not be used for
+        #      model evaluation because we don't have the labels for them. Instead, we
+        #      will submit the test set predictions to the competition site.
+        #
+        # We will need to make scenes for each of the 11,481 test chips. It is possible
         # to do this by reading the test STAC directly from s3 but it is not recommended.
-        # A better option would be to download the test data from the competition site 
-        # and uncompress it into the data directory in this repo. That directory will 
+        # A better option would be to download the test data from the competition site
+        # and uncompress it into the data directory in this repo. That directory will
         # mount into `/opt/data/` within the docker container and then you can supply
-        # uri for the test catalog json as the `test_stac_uri` parameter of this experiment 
+        # uri for the test catalog json as the `test_stac_uri` parameter of this experiment
         # runner (i.e. `/opt/data/test/catalog.json`).
-        # 
+        #
         # It may make sense to start with a work flow in which you hold off on making
         # predictions on the test set until you have tried several approaches and validated
         # evaluated them against the validation set. If you would like to do that you can
         # simply remove the `.with_test_scenes()` call from the dataset config builder as well
-        # as previous steps that deal with test data. 
-        
+        # as previous steps that deal with test data.
+
         # Create a generator of all test images
         test_stac = Catalog.from_file(test_stac_uri)
-        all_test_items = test_stac.get_all_items()            
+        all_test_items = test_stac.get_all_items()
 
-        # By default, this script will look for test images in the file tree starting at 
+        # By default, this script will look for test images in the file tree starting at
         # the location of the test STAC catalog. However, in this example we want to access
         # the catalog json and the images from different locations. As mentioned above, in the
         # interest of time we will get all test ids from a local version of the test STAC but
         # will still need to download the images directly from s3 since the prediction will
-        # run remotely. We will use a different location by setting the `test_img_dir` 
+        # run remotely. We will use a different location by setting the `test_img_dir`
         # command line parameter to the poblic location of the test data on s3 (i.e.
         # -a test_img_dir s3://drivendata-competition-building-segmentation/test/)
         if not test_img_dir:
             test_img_dir = dirname(test_stac_uri)
 
-        # Configure chip creation 
+        # Configure chip creation
         chip_opts = {
-            'window_method': 'sliding', # use sliding window method of to create chips
+            'window_method': 'sliding',  # use sliding window method of to create chips
             'stride': 300               # slide over 300px to generate each new chip
         }
 
-        # Training configuration: try to improve performance by tuning these 
+        # Training configuration: try to improve performance by tuning these
         # hyperparameters
         config = {
             'batch_size': 8,         # 8 chips per batch
             'num_epochs': 6,         # complete 6 epochs
             'debug': True,           # produce example chips to help with debugging
-            'lr': 1e-4,              # set learning 
+            'lr': 1e-4,              # set learning
             'one_cycle': True,       # use cyclic learning rate scheduler
-            'model_arch': 'resnet18' # model architecture
+            'model_arch': 'resnet18'  # model architecture
         }
 
         # It is generally helpful to try running a 'test' version of the experiment before
@@ -107,19 +107,38 @@ class BenchmarkExperiment(rv.ExperimentSet):
             config['batch_size'] = 2
             config['num_epochs'] = 1
 
+            # In this 'test' scenario, we will generate a small number of chips at random
+            # rather than creating a set that exhaustively covers each scene
             chip_opts = {
                 'window_method': 'random_sample',
                 'chips_per_scene': 10
             }
 
+            # Convert the generator of all items (i.e. chips) within the test set into
+            # a list of only five
             all_test_items = [next(all_test_items) for _ in range(5)]
 
+            # Modify the experiment ID so that a future iteration of this same experiment
+            # without the 'test' flag will not rely on the same outputs
             experiment_id += '-TEST'
-            
-        def make_train_scenes(item):
-            area = item.get_parent().id
-            label_uri = join(dirname(train_stac_uri), area, '{}-labels'.format(item.id), '{}.geojson'.format(item.id))
 
+        # Rastervision stores information about images and often their associted labels in
+        # SceneConfig objects. We will use this function to created lists of train and
+        # validation scenes from pystac items.
+        def make_train_scenes(item):
+            # We can easily construct the uri using the root directory of the training STAC.
+            # Each rv scene will consist of one portion of an image created in the preprocessing
+            # stage but it is not necessary to split the labels up in the same way. We can
+            # pass a scene's label uri to all of it's child images and rv will subset the
+            # labels automatically.
+            area = item.get_parent().id
+            label_uri = join(dirname(train_stac_uri), area,
+                             '{}-labels'.format(item.id), '{}.geojson'.format(item.id))
+
+            # If you preprocessed the imagery using the 'PREPROCESS' aux command, the
+            # image splits will have integer suffixes that ascend from 0 until the image
+            # has been completely covered. To make sure we get all of the splits we will
+            # incrementally create new uri's until one is found to not exist.
             i = 0
             images_remaining = True
             scenes = []
@@ -128,11 +147,18 @@ class BenchmarkExperiment(rv.ExperimentSet):
                                   '{}_{}.tif'.format(item.id, i))
 
                 if file_exists(raster_uri):
+                    # construct a raster source (i.e. the image)
                     raster_source = rv.RasterSourceConfig.builder(rv.RASTERIO_SOURCE) \
                         .with_uri(raster_uri) \
                         .with_channel_order([0, 1, 2]) \
                         .build()
 
+                    # construct a label source (i.e. the scene's geojson labels)
+                    # The with_rasterizer_options method sets the default pixel value to use
+                    # for background pixels in prediction. The value of 0 is reserved for nodata
+                    # pixels in rv so we will need to add a postprocessing step at the end
+                    # in order for the test set predicitons to match the competition submission
+                    # suidelines.
                     label_raster_source = rv.RasterSourceConfig.builder(rv.RASTERIZED_SOURCE) \
                         .with_vector_source(label_uri) \
                         .with_rasterizer_options(2) \
@@ -142,6 +168,7 @@ class BenchmarkExperiment(rv.ExperimentSet):
                         .with_raster_source(label_raster_source) \
                         .build()
 
+                    # Build scene config
                     scene = rv.SceneConfig.builder() \
                         .with_task(task) \
                         .with_id('{}_{}'.format(item.id, i)) \
@@ -156,6 +183,8 @@ class BenchmarkExperiment(rv.ExperimentSet):
 
             return scenes
 
+        # The test image chips are also encoded as SceneConfig objects but
+        # lack a label source
         def make_test_scene(item):
             raster_uri = join(test_img_dir, item.id, '{}.tif'.format(item.id))
 
@@ -171,33 +200,46 @@ class BenchmarkExperiment(rv.ExperimentSet):
 
             return scene
 
+        # Configure a semantic segmentation task with classes defined in constants.py,
+        # use the chipping options defined previously within this script
         task = rv.TaskConfig.builder(rv.SEMANTIC_SEGMENTATION) \
                             .with_classes(CLASSES) \
                             .with_chip_options(**chip_opts) \
                             .build()
 
+        # This semantic segmentation task uses pytorch as a backend deep learning library.
+        # The pytorch backend comes preloaded in rv and installed in this docker container
+        # but you can use any backend with additional configuration. Configure the experiment
+        # to use the training parameters defined above.
         backend = rv.BackendConfig.builder(PYTORCH_SEMANTIC_SEGMENTATION) \
                                   .with_task(task) \
                                   .with_train_options(**config) \
                                   .build()
 
+        # Create train, validation and test scenes
         print('Creating train scenes')
         train_scenes = reduce(
             lambda a, b: a + b, [make_train_scenes(train_stac.get_child(c).get_item(i)) for c, i in train_ids])
+
         print('Creating validation scenes')
         valid_scenes = reduce(
             lambda a, b: a + b, [make_train_scenes(train_stac.get_child(c).get_item(i)) for c, i in valid_ids])
+        
+        # Using four different scenes (from four different cities) for the validation
+        # set is benefficial becuase it gives us a diverse set of imagery to use when 
+        # determining the generalizability of the model. However, it would be overkill
+        # to validate the model on the entirety of the four scenes. We will take a random
+        # sample of those image splits and validate on those.
+        valid_scenes = sample(valid_scenes, 30)
+
         print('Creating test scenes')
-
-        # test_stac_dir = dirname(test_stac_uri)
-        # test_stac_dir = 's3://drivendata-competition-building-segmentation/test/'        
-
         test_scenes = [make_test_scene(item) for item in all_test_items]
 
         if test:
             train_scenes = sample(train_scenes, 3)
             valid_scenes = sample(valid_scenes, 3)
 
+        # and use them as inputs to an RV DatasetConfig
         print('Building dataset config')
         dataset = rv.DatasetConfig.builder() \
             .with_train_scenes(train_scenes) \
@@ -205,6 +247,17 @@ class BenchmarkExperiment(rv.ExperimentSet):
             .with_test_scenes(test_scenes) \
             .build()
 
+        # As mentioned in a previous comment, 0 is a reserved pixel value in rv and can therefore
+        # not be used as the predicted background (or 'no building' value). Those pixels will have
+        # a value of 2 in the raw predictions but we need to convert them to 0 to match the
+        # submission guidelines.
+        #
+        # We can do this using a simple aux command called 'POSTPROCESS' that is defined in
+        # aux/postprocess.py. This command requires a few parameters that we define in the dict
+        # below. It takes the raw prediction tifs as input. These are not created yet but
+        # will be written to a specific location below the root uri. Raster Vision will understand
+        # that the POSTPROCESS task relies on the output of the predict task and will therefore
+        # wait to run the former until the latter has completed.
         postprocess_config = {
             'POSTPROCESS': {
                 'key': 'postprocess',
@@ -216,6 +269,9 @@ class BenchmarkExperiment(rv.ExperimentSet):
             }
         }
 
+        # Finally build an experiment from all of these constituent parts. Returning an
+        # methods that have the prefix 'exp_' (like this one) will run reflexively when this
+        # script is run.
         experiment = rv.ExperimentConfig.builder() \
             .with_id(experiment_id) \
             .with_task(task) \
@@ -225,4 +281,6 @@ class BenchmarkExperiment(rv.ExperimentSet):
             .with_custom_config(postprocess_config) \
             .build()
 
+        # returning the experiment config will kick off the chain workflow if running locally
+        # or submit the sequence of dependent jobs if running on batch
         return experiment
